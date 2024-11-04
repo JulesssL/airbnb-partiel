c14deaf4bb552d40649a9fd26a7bc2b56ef4b7f1
JulesssL <julesleroux15@gmail.com>
2024-11-04 16:31:13 +0100

    Ajout de la fonctionnalité pour faciliter la navigation

--- app.js
+++ app.js
@@ -2,7 +2,23 @@ function print(a) {
     console.log(a)
 }
 
+// Explication du Site
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        toggleExplication();
+    }
+});
+
+
+// Évènements
 
 document.addEventListener('DOMContentLoaded', () => {
-    
+
 })
+
+
+// Fonctions
+
+function toggleExplication() {
+    document.querySelector('.explication-site').classList.toggle('none');
+}
